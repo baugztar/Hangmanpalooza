fadbf7fbceacb691cfd4e0552bb0493c70e7b6e0
instantman <maddoxx91@gmail.com>
2015-09-15 13:36:33 +0200

    lagt til

---
+++ HangmanApp/app/src/androidTest/java/s198527/s180859/overjordet/bauge/hangmanapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package s198527.s180859.overjordet.bauge.hangmanapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
